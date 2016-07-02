494455700ebc05067f4c7e0d0da02938074f06f5
Dipesh Kumar Trikam <dtri723@aucklanduni.ac.nz>
2016-07-02 14:22:35 +1200

    cleaned up code

--- index.php
+++ index.php
@@ -21,16 +21,7 @@
       /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
     }
   </style>
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
-
 <body>
 
   <!-- Navigation -->
@@ -60,9 +51,7 @@
           </li>
         </ul>
       </div>
-      <!-- /.navbar-collapse -->
     </div>
-    <!-- /.container -->
   </nav>
 
   <!-- Select List -->
@@ -70,10 +59,6 @@
 
   <!-- Page Content -->
   <div class="container">
-
-
-
-
     <form class="form" role="form" method="POST" action="<?php ECHO $_SERVER['PHP_SELF']; ?>">
       <div class="well well-sm row">
 
@@ -175,11 +160,6 @@
       </div>
     </div>
   </div>
-
-
-
-
-
   <!-- /.container -->
 
   <!-- jQuery Version 1.11.1 -->
